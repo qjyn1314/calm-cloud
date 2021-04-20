b1bbb33a86c42ef922b73af5609e15e5c3ad2fb1
wangjunming <qjyn1314@163.com>
2021-04-20 14:28:47 +0800

    添加初始化SQL

---
+++ calm-core/src/main/resources/calm_deploy.sql
@@ -0,0 +1,175 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地MYSQL数据库-123456
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : calm
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : 65001
+
+ Date: 20/04/2021 14:28:10
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for gen_datasource_conf
+-- ----------------------------
+DROP TABLE IF EXISTS `gen_datasource_conf`;
+CREATE TABLE `gen_datasource_conf`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '数据库名',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '数据库连接URL',
+  `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+  `create_date` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `update_date` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '数据源表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu`  (
+  `menu_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '菜单编号-唯一',
+  `pcode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单父编号',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单名称',
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
+  `font_family` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单样式',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'url地址',
+  `sort` int(3) NULL DEFAULT NULL COMMENT '菜单排序号',
+  `menu_type` int(1) NULL DEFAULT NULL COMMENT '菜单类型：1-菜单；2-目录',
+  `status` int(1) NULL DEFAULT NULL COMMENT '菜单状态：1-启用；0-停用',
+  `open_flag` int(1) NULL DEFAULT NULL COMMENT '是否打开：1-是；0-否',
+  `system_type` int(1) NULL DEFAULT NULL COMMENT '系统分类：1-后台管理系统；2-前台管理系统',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  `create_user` bigint(20) NULL DEFAULT NULL COMMENT '创建人',
+  `update_user` bigint(20) NULL DEFAULT NULL COMMENT '修改人',
+  PRIMARY KEY (`menu_id`, `code`) USING BTREE,
+  INDEX `index_code`(`code`, `name`) USING BTREE COMMENT 'code和名字的索引'
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '菜单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES (1, '0', '-1', '顶级', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `sys_menu` VALUES (7, 'console', '0', '控制台', '&#xe654;', 'ok-icon', 'page/console.html', 0, 1, 1, 1, 1, '默认的控制台', '2021-04-19 09:51:01', '2021-04-19 09:51:01', 1, 1);
+INSERT INTO `sys_menu` VALUES (8, 'member', '0', '会员管理', '&#xe66f;', 'layui-icon', '', 1, 1, 1, 0, 1, '后台系统-会员管理', '2021-04-19 10:08:53', '2021-04-19 10:08:53', 1, 1);
+INSERT INTO `sys_menu` VALUES (9, 'user', 'member', '用户列表', '&#xe62e;', 'layui-icon', 'page/member/user-list.html', 2, 2, 1, 0, 1, '后台系统-用户列表', '2021-04-19 10:28:50', '2021-04-19 10:31:21', 1, 1);
+INSERT INTO `sys_menu` VALUES (10, 'role', 'member', '角色列表', '&#xe62e;', 'layui-icon', 'page/member/role-list.html', 3, 2, 1, 0, 1, '后台系统-角色列表', '2021-04-19 10:30:09', '2021-04-19 10:30:09', 1, 1);
+INSERT INTO `sys_menu` VALUES (11, 'permission', 'member', '菜单列表', '&#xe62e;', 'layui-icon', 'page/member/permission-list.html', 4, 2, 1, 0, 1, '后台系统-菜单列表', '2021-04-19 10:31:03', '2021-04-20 05:51:56', 1, 1);
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `role_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色编码',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '提示',
+  `sort` int(11) NULL DEFAULT NULL COMMENT '序号',
+  `version` int(11) NULL DEFAULT NULL COMMENT '乐观锁',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  `create_user` bigint(20) NULL DEFAULT NULL COMMENT '创建用户',
+  `update_user` bigint(20) NULL DEFAULT NULL COMMENT '修改用户',
+  PRIMARY KEY (`role_id`, `code`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES (9, 'system_admin', '超级管理员', '具备所有菜单', 0, NULL, '2021-04-19 09:27:25', '2021-04-19 09:27:25', 1, 1);
+
+-- ----------------------------
+-- Table structure for sys_role_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu`  (
+  `relation_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `role_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色编码',
+  `menu_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '菜单编码',
+  PRIMARY KEY (`relation_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 41 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色和菜单关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role_menu
+-- ----------------------------
+INSERT INTO `sys_role_menu` VALUES (35, 'system_admin', '0');
+INSERT INTO `sys_role_menu` VALUES (36, 'system_admin', 'console');
+INSERT INTO `sys_role_menu` VALUES (37, 'system_admin', 'member');
+INSERT INTO `sys_role_menu` VALUES (38, 'system_admin', 'user');
+INSERT INTO `sys_role_menu` VALUES (39, 'system_admin', 'role');
+INSERT INTO `sys_role_menu` VALUES (40, 'system_admin', 'permission');
+
+-- ----------------------------
+-- Table structure for sys_sequence
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_sequence`;
+CREATE TABLE `sys_sequence`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '序列号',
+  `sequence_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '序列号type',
+  `sequence` bigint(50) NULL DEFAULT NULL COMMENT '序列号',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`, `sequence_type`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `account` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '账号',
+  `code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户编码',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `salt` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'md5密码盐',
+  `name` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '名字',
+  `birthday` datetime(0) NULL DEFAULT NULL COMMENT '生日',
+  `sex` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '2' COMMENT '性别:0-女；1-男；2-未知',
+  `email` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '电子邮件',
+  `phone` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '电话',
+  `remarks` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '状态：0-待审核；1-已审核；2-已停用',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `create_user` bigint(20) NULL DEFAULT NULL COMMENT '创建人',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `update_user` bigint(20) NULL DEFAULT NULL COMMENT '更新人',
+  `version` int(11) NULL DEFAULT NULL COMMENT '乐观锁',
+  PRIMARY KEY (`user_id`, `account`, `code`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES (1, NULL, 'qjyn1314@163.com', '001', '$2a$05$JVk3xVHY/FUsf0Ps4hAjFemimUh0dHlBCJuCt4U/YausKMtN4XGxG', '$2a$05$JVk3xVHY/FUsf0Ps4hAjFe', '排骨-2019-11', '2021-04-08 14:28:53', '1', 'qjyn1314@163.com', '15515014022', '', '1', '2021-04-08 14:29:15', 1, '2021-04-19 05:31:45', 1, 1);
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role`  (
+  `ur_id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `user_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户编码',
+  `role_code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色编码',
+  PRIMARY KEY (`ur_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 18 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_user_role
+-- ----------------------------
+INSERT INTO `sys_user_role` VALUES (17, '001', 'system_admin');
+
+SET FOREIGN_KEY_CHECKS = 1;
