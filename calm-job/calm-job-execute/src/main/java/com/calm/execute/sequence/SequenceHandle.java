234a41551be92c801069f11cbd5cd189dfa6a565
wangjunming <qjyn1314@163.com>
2021-07-18 10:46:46 +0800

    添加job执行任务工程

---
+++ calm-job/calm-job-execute/src/main/java/com/calm/execute/sequence/SequenceHandle.java
@@ -0,0 +1,14 @@
+package com.calm.execute.sequence;
+
+/**
+ * <p>
+ * explain: 序号服务对应的
+ * </p>
+ *
+ * @author wangjunming
+ * @since 2021/7/18 10:14
+ */
+public class SequenceHandle {
+
+
+}
---
+++ calm-job/calm-job-execute/src/test/java/com/calm/execute/CalmJobExecuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calm.execute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmJobExecuteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
